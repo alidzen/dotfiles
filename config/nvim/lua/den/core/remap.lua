9d7b45e5cb4cc642e3a79944d9663ec2fadaeed6
denisgolubtsov <d.s.golubtsov@gmail.com>
2023-02-16 17:33:59 +0100

    WIP. Change nvim structure

---
+++ config/nvim/lua/den/core/remap.lua
@@ -0,0 +1,44 @@
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- copy/paste from OS buffer
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"_pD')
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
+
+-- move highlighted strings up/down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- delte whole string
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- disable accidentally close terminal
+vim.keymap.set("n", "Q", "<nop>")
+
+-- format with lsp
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quick list navigation fix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
