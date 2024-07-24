f210a705b06f996b9538a4a420413a4bbfae5b62
Denis Golubtsov <d.s.golubtsov@gmail.com>
2024-07-24 11:21:40 +0200

    add git branch for dirs

--- config/nvim/lua/plugins/flatten.lua
+++ config/nvim/lua/plugins/flatten.lua
@@ -2,6 +2,7 @@ return {
   {
     "willothy/flatten.nvim",
     config = true,
+
     -- or pass configuration with
     -- opts = {  }
     -- Ensure that it runs first to minimize delay when opening file from terminal
