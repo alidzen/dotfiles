dc98432bf07ada70eb6ff8b40367acb7a54e9abd
Denis Golubtsov <d.s.golubtsov@gmail.com>
2024-08-26 22:52:32 +0200

    add QX keymaps, add kube functions

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,6 @@
 vim.keymap.set("i", "jk", "<Esc>", { silent = true })
 
 vim.keymap.set("n", "<leader>br", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "[l", "<cmd>colder<cr>", { desc = "Prev QF error list" })
+vim.keymap.set("n", "]l", "<cmd>cnewer<cr>", { desc = "Next QF error list" })
